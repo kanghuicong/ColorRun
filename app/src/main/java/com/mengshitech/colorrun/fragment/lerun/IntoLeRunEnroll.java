b906d56f77ac6f3f8b9fdc90e929626a700cb620
chenyongyin <wschenyongyin@qq.com>
2016-07-30 16:54:35 +0800

    报名页面实现了

--- app/src/main/java/com/mengshitech/colorrun/fragment/lerun/IntoLeRunEnroll.java
+++ app/src/main/java/com/mengshitech/colorrun/fragment/lerun/IntoLeRunEnroll.java
@@ -1,13 +1,25 @@
 package com.mengshitech.colorrun.fragment.lerun;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -17,13 +29,21 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mengshitech.colorrun.MainActivity;
 import com.mengshitech.colorrun.R;
 import com.mengshitech.colorrun.bean.UserEntiy;
+import com.mengshitech.colorrun.customcontrols.ChoseImageDiaLog;
 import com.mengshitech.colorrun.utils.HttpUtils;
 import com.mengshitech.colorrun.utils.IPAddress;
 import com.mengshitech.colorrun.utils.MainBackUtility;
 import com.mengshitech.colorrun.utils.Utility;
+import com.mengshitech.colorrun.utils.upload;
 
+import android.view.View.OnClickListener;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,7 +51,7 @@ import java.util.Map;
  * Created by kanghuicong on 2016/7/27  17:58.
  * 515849594@qq.com
  */
-public class IntoLeRunEnroll extends AppCompatActivity {
+public class IntoLeRunEnroll extends AppCompatActivity implements android.view.View.OnClickListener {
     TextView enroll_name, enroll_time, enroll_address;
     EditText user_name, card_number, enroll_unit, enroll_number;
     RadioGroup rg_enroll_sex, rg_enroll_size;
@@ -40,8 +60,16 @@ public class IntoLeRunEnroll extends AppCompatActivity {
     ListView enroll_price;
     RadioButton enroll_agree, rb_sex_man, rb_sex_woman, rb_size_s, rb_size_m, rb_size_l, rb_size_xl, rb_size_2xl, rb_size_3xl;
     Button bt_enroll_agree;
-    String sex, size;
+    String sex="男";
+    String size;
     ImageView image;
+    ChoseImageDiaLog dialog;
+    String imageFilePath;
+    File temp;
+    //图片上传成功后返回的图片路径
+    String ScuessImagePath;
+    String user_id;
+    int lerun_id;
 
 
     @Override
@@ -56,6 +84,11 @@ public class IntoLeRunEnroll extends AppCompatActivity {
     }
 
     private void FindId() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("user_type", Activity.MODE_PRIVATE);
+        user_id = sharedPreferences.getString("user_id", "");
+
+
         enroll_name = (TextView) findViewById(R.id.tv_enroll_university);
         enroll_time = (TextView) findViewById(R.id.tv_enroll_time);
         enroll_address = (TextView) findViewById(R.id.tv_enroll_add);
@@ -78,71 +111,84 @@ public class IntoLeRunEnroll extends AppCompatActivity {
         enroll_authentication = (ImageView) findViewById(R.id.iv_enroll_authentication);
         enroll_agree = (RadioButton) findViewById(R.id.rb_enroll_agree);
         bt_enroll_agree = (Button) findViewById(R.id.bt_enroll_agree);
+
+        enroll_authentication.setOnClickListener(this);
     }
 
     private void EnrollClick() {
         bt_enroll_agree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if ("".equals(user_name.getText().toString()) ||
-                        "".equals(card_number.getText().toString()) ||
-                        "".equals(enroll_unit.getText().toString()) ||
-                        "".equals(enroll_number.getText().toString()) ||
-                        "请选择".equals(enroll_spinner_card.getSelectedItem().toString()) ||
-                        "请选择".equals(enroll_spinner_id.getSelectedItem().toString())) {
-                    Toast.makeText(IntoLeRunEnroll.this, "请将信息填写完整！", Toast.LENGTH_SHORT).show();
-                } else {
-                    if ("本校学生".equals(enroll_spinner_id.getSelectedItem().toString())) {
-                        if (enroll_authentication.getDrawable().getCurrent().getConstantState().equals(getResources().getDrawable(R.mipmap.enroll_add_image).getConstantState())) {
-                            Toast.makeText(IntoLeRunEnroll.this, "请将学生证图片上传！", Toast.LENGTH_SHORT).show();
-                        } else {
+                                               @Override
+                                               public void onClick(View v) {
+                                                   if ("".equals(user_name.getText().toString()) ||
+                                                           "".equals(card_number.getText().toString()) ||
+                                                           "".equals(enroll_unit.getText().toString()) ||
+                                                           "".equals(enroll_number.getText().toString()) ||
+                                                           "请选择".equals(enroll_spinner_card.getSelectedItem().toString()) ||
+                                                           "请选择".equals(enroll_spinner_id.getSelectedItem().toString())) {
+                                                       Toast.makeText(IntoLeRunEnroll.this, "请将信息填写完整！", Toast.LENGTH_SHORT).show();
+                                                   } else {
+                                                       if ("本校学生".equals(enroll_spinner_id.getSelectedItem().toString())) {
 
-                        }
-                    } else {
-                    }
+                                                           if (enroll_authentication.getDrawable().getCurrent().getConstantState().equals(getResources().getDrawable(R.mipmap.enroll_add_image).getConstantState())) {
+                                                               if ("本校学生".equals(enroll_spinner_id.getSelectedItem().toString()))
+                                                                   Toast.makeText(IntoLeRunEnroll.this, "请将学生证图片上传！", Toast.LENGTH_SHORT).show();
+                                                           } else {
 
-                    if (rb_sex_man.isChecked()) {
-                        sex = "男";
-                    } else if (rb_sex_woman.isChecked()) {
-                        sex = "女";
-                    }
-                    if (rb_size_s.isChecked()) {
-                        size = "S";
-                    } else if (rb_size_m.isChecked()) {
-                        size = "M";
-                    } else if (rb_size_l.isChecked()) {
-                        size = "L";
-                    } else if (rb_size_xl.isChecked()) {
-                        size = "XL";
-                    } else if (rb_size_2xl.isChecked()) {
-                        size = "2XL";
-                    } else if (rb_size_3xl.isChecked()) {
-                        size = "3XL";
-                    }
-                }
-                new Thread(runnable).start();
-            }
-        });
+                                                           }
+                                                       } else if (rb_sex_man.isChecked()) {
+                                                           sex = "男";
+                                                       } else if (rb_sex_woman.isChecked()) {
+                                                           sex = "女";
+                                                       } else if (rb_size_s.isChecked()) {
+                                                           size = "S";
+                                                       } else if (rb_size_m.isChecked()) {
+                                                           size = "M";
+                                                       } else if (rb_size_l.isChecked()) {
+                                                           size = "L";
+                                                       } else if (rb_size_xl.isChecked()) {
+                                                           size = "XL";
+                                                       } else if (rb_size_2xl.isChecked()) {
+                                                           size = "2XL";
+                                                       } else if (rb_size_3xl.isChecked()) {
+                                                           size = "3XL";
+                                                       }
+                                                       new Thread(uploadRunnable).start();
+                                                   }
+                                               }
+                                           }
+
+        );
     }
 
+    //执行报名的请求的线程
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
+            String signin_type;
+            if (enroll_spinner_card.getSelectedItem().toString().equals("本校学生")) {
+                signin_type = "1";
+            } else {
+                signin_type = "2";
+            }
             String path = IPAddress.PATH;
             Map<String, String> map = new HashMap<String, String>();
+            map.put("flag", "lerun");
             map.put("index", "4");
-            map.put("user_id", "123");
-            map.put("lerun_id", "1");
+            map.put("user_id", user_id);
+            map.put("lerun_id", "999");
             map.put("user_telphone", enroll_number.getText().toString());
             map.put("lerun_title", enroll_name.getText().toString());
-            map.put("signin_type ", enroll_spinner_id.getSelectedItem().toString());
+            map.put("signin_type", signin_type);
+
             map.put("personal_name", user_name.getText().toString());
             map.put("company_name", enroll_unit.getText().toString());
 //            map.put("certificate_image",enroll_authentication);
             map.put("identity_type", enroll_spinner_card.getSelectedItem().toString());
             map.put("identity_card", card_number.getText().toString());
             map.put("dress_size", size);
-            map.put("payment", "");
+            map.put("payment", "69");
+            map.put("certificate_image", ScuessImagePath);
+            map.put("user_sex",sex );
 
             String result = HttpUtils.sendHttpClientPost(path, map,
                     "utf-8");
@@ -151,7 +197,7 @@ public class IntoLeRunEnroll extends AppCompatActivity {
             handler.sendMessage(msg);
         }
     };
-
+    //执行报名的请求的handler
     Handler handler = new Handler() {
 
         public void handleMessage(Message msg) {
@@ -161,15 +207,175 @@ public class IntoLeRunEnroll extends AppCompatActivity {
             if (result.equals("timeout")) {
 //                progressDialog.dismiss();
                 Toast.makeText(IntoLeRunEnroll.this, "连接服务器超时", Toast.LENGTH_SHORT).show();
-            }else if(result.equals("0")){
+            } else if (result.equals("0")) {
                 Toast.makeText(IntoLeRunEnroll.this, "报名失败...", Toast.LENGTH_SHORT).show();
-            }else if(result.equals("1")){
+            } else if (result.equals("1")) {
                 Toast.makeText(IntoLeRunEnroll.this, "您已经报过名啦", Toast.LENGTH_SHORT).show();
-            }else if(result.equals("2")){
+            } else if (result.equals("2")) {
                 Toast.makeText(IntoLeRunEnroll.this, "报名成功，等待审核通过", Toast.LENGTH_SHORT).show();
                 finish();
             }
 
         }
     };
+    //执行上传证件照的线程
+    Runnable uploadRunnable = new Runnable() {
+        @Override
+        public void run() {
+            String servletPath = IPAddress.ImagePath;
+            ScuessImagePath = upload.uploadImage(temp, servletPath);
+            Message msg = new Message();
+            msg.obj = ScuessImagePath;
+            uploadHandle.sendMessage(msg);
+
+        }
+    };
+    Handler uploadHandle = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String imagepath = (String) msg.obj;
+            if (imagepath.equals("failure")) {
+                Toast.makeText(IntoLeRunEnroll.this, "图片上传失败", Toast.LENGTH_SHORT).show();
+            } else {
+                new Thread(runnable).start();
+            }
+        }
+    };
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //上传证件照图片按钮
+            case R.id.iv_enroll_authentication:
+                showDailog();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void showDailog() {
+        dialog = new ChoseImageDiaLog(IntoLeRunEnroll.this, R.layout.dialog_choseimage,
+                R.style.dialog, new ChoseImageDiaLog.LeaveMyDialogListener() {
+
+            @Override
+
+            public void onClick(View view) {
+
+                System.out.println("aaaaaaaaaaaaaa");
+                switch (view.getId()) {
+                    case R.id.btn_takephoto:
+
+                        imageFilePath = Environment
+                                .getExternalStorageDirectory()
+                                .getAbsolutePath()
+                                + "/filename.jpg";
+                        temp = new File(imageFilePath);
+                        Uri imageFileUri = Uri.fromFile(temp);// 获取文件的Uri
+                        Intent it = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);// 跳转到相机Activity
+                        it.putExtra(
+                                android.provider.MediaStore.EXTRA_OUTPUT,
+                                imageFileUri);// 告诉相机拍摄完毕输出图片到指定的Uri
+                        startActivityForResult(it, 102);
+                        dialog.dismiss();
+                        break;
+                    case R.id.btn_picture:
+                        Intent intent1 = new Intent(Intent.ACTION_PICK
+                        );
+                        intent1.setDataAndType(
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                        startActivityForResult(intent1, 103);
+                        dialog.dismiss();
+
+                        break;
+                    case R.id.btn_cancel:
+
+                        dialog.dismiss();
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        });
+        // 设置dialog弹出框显示在底部，并且宽度和屏幕一样
+        Window window = dialog.getWindow();
+        dialog.show();
+        window.setGravity(Gravity.BOTTOM);
+        window.getDecorView().setPadding(0, 0, 0, 0);
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.width = WindowManager.LayoutParams.FILL_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setAttributes(lp);
+    }
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 102:
+                if (resultCode == AppCompatActivity.RESULT_OK) {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 2;
+                    Bitmap bmp = BitmapFactory.decodeFile(imageFilePath, options);
+
+//                    Bitmap bmp = BitmapFactory.decodeFile(imageFilePath);
+                    enroll_authentication.setImageBitmap(bmp);
+
+                }
+                break;
+            case 103:
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 2;
+
+                // 外界的程序访问ContentProvider所提供数据 可以通过ContentResolver接口
+                ContentResolver resolver = getContentResolver();
+
+
+                if (data != null) {
+                    Uri originalUri = data.getData(); // 获得图片的uri
+
+                    try {
+                        Bitmap bm = MediaStore.Images.Media.getBitmap(resolver, originalUri); // 显得到bitmap图片
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    // 这里开始的第二部分，获取图片的路径：
+
+                    String[] proj = {MediaStore.Images.Media.DATA};
+
+                    // 好像是android多媒体数据库的封装接口，具体的看Android文档
+                    @SuppressWarnings("deprecation")
+                    Cursor cursor = managedQuery(originalUri, proj, null, null,
+                            null);
+                    // 按我个人理解 这个是获得用户选择的图片的索引值
+                    int column_index = cursor
+                            .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    // 将光标移至开头 ，这个很重要，不小心很容易引起越界
+                    cursor.moveToFirst();
+                    // 最后根据索引值获取图片路径
+                    imageFilePath = cursor.getString(column_index);
+                    Bitmap bmp = BitmapFactory.decodeFile(imageFilePath, options);
+                    enroll_authentication.setImageBitmap(bmp);
+
+                    temp = new File(imageFilePath);
+
+                    System.out.println(imageFilePath);
+                }
+
+
+                break;
+        }
+
+    }
+
 }
--- app/src/main/java/com/mengshitech/colorrun/utils/upload.java
+++ app/src/main/java/com/mengshitech/colorrun/utils/upload.java
@@ -1,8 +1,16 @@
 package com.mengshitech.colorrun.utils;
 //有问题发邮箱:wschenyongyin@qq.com
+
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -14,55 +22,141 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
 
 
 import android.util.Log;
 
 public class upload {
-	private static String requestURL = "http://xxxx这里为你的ip地址xxxx:8080/ServletForUpload/servlet/ImageUploadServlet"; // 服务器的URL
-	private static final String CHARSET = "UTF-8"; // 设置编码
-	private int readTimeOut = 10 * 1000; // 读取超时
-	private int connectTimeout = 10 * 1000; // 超时时间
-	private int mStatus; // 返回状态码
+
+    private static final String CHARSET = "UTF-8"; // 设置编码
+    private static int readTimeOut = 10 * 1000; // 读取超时
+    private static int connectTimeout = 10 * 1000; // 超时时间
+    private static int mStatus; // 返回状态码
+    private static final int TIME_OUT = 10 * 1000;
+    private static HttpResponse resp;
+    private static String result = null;
 
 
-	public synchronized void postMethod(List<String> imageUrlList) {
-		try {
-			String[] filePath = new String[imageUrlList.size()];
+    public static synchronized String uploadListImage(List<String> imageUrlList, String ServletPath) throws IOException {
+        try {
+            String[] filePath = new String[imageUrlList.size()];
 
-			int size = imageUrlList.size();
-			for (int i = 0; i < size; i++) {
-				filePath[i] = imageUrlList.get(i);
-			}
+            int size = imageUrlList.size();
+            for (int i = 0; i < size; i++) {
+                filePath[i] = imageUrlList.get(i);
+            }
 
-			// 链接超时，请求超时设置
-			BasicHttpParams httpParams = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParams,
-					connectTimeout);
-			HttpConnectionParams.setSoTimeout(httpParams, readTimeOut);
+            // 链接超时，请求超时设置
+            BasicHttpParams httpParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams,
+                    connectTimeout);
+            HttpConnectionParams.setSoTimeout(httpParams, readTimeOut);
 
-			// 请求参数设置
-			HttpClient client = new DefaultHttpClient(httpParams);
-			HttpPost post = new HttpPost(requestURL);
-			MultipartEntity entity = new MultipartEntity();
+            // 请求参数设置
+            HttpClient client = new DefaultHttpClient(httpParams);
+            HttpPost post = new HttpPost(ServletPath);
+            MultipartEntity entity = new MultipartEntity();
 
-			
 
-			// 
-			for (String p : filePath) {
-				entity.addPart("fileimg", new FileBody(new File(p), "image/*"));
-			}
+            //
+            for (String p : filePath) {
+                entity.addPart("fileimg", new FileBody(new File(p), "image/*"));
+            }
 
-			post.setEntity(entity);
-			HttpResponse resp = client.execute(post);
-			// 获取回调值
-			Log.e("", EntityUtils.toString(resp.getEntity()));
-			mStatus = resp.getStatusLine().getStatusCode();
-			Log.e("返回的值为", mStatus + "");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            post.setEntity(entity);
+            resp = client.execute(post);
+            // 获取回调值
+            Log.e("", EntityUtils.toString(resp.getEntity()));
+            mStatus = resp.getStatusLine().getStatusCode();
+            if (mStatus == 200) {
+                result = EntityUtils.toString(resp.getEntity());
+            } else {
+                result = "failure";
+            }
+            Log.e("返回的值为", mStatus + "");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return EntityUtils.toString(resp.getEntity());
+    }
+
+    //传file文件  和servlet地址
+    public static String uploadImage(File file, String ServletPath) {
+
+        String BOUNDARY = UUID.randomUUID().toString();  //边界标识   随机生成
+        String PREFIX = "--", LINE_END = "\r\n";
+        String CONTENT_TYPE = "multipart/form-data";   //内容类型
+
+        try {
+            URL url = new URL(ServletPath);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(TIME_OUT);
+            conn.setConnectTimeout(TIME_OUT);
+            conn.setDoInput(true);  //允许输入流
+            conn.setDoOutput(true); //允许输出流
+            conn.setUseCaches(false);  //不允许使用缓存
+            conn.setRequestMethod("POST");  //请求方式
+            conn.setRequestProperty("Charset", CHARSET);  //设置编码
+            conn.setRequestProperty("connection", "keep-alive");
+            conn.setRequestProperty("Content-Type", CONTENT_TYPE + ";boundary=" + BOUNDARY);
+            conn.connect();
+
+            if (file != null) {
+                /**
+                 * 当文件不为空，把文件包装并且上传
+                 */
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+                StringBuffer sb = new StringBuffer();
+                sb.append(PREFIX);
+                sb.append(BOUNDARY);
+                sb.append(LINE_END);
+                /**
+                 * 这里重点注意：
+                 * name里面的值为服务器端需要key   只有这个key 才可以得到对应的文件
+                 * filename是文件的名字，包含后缀名的   比如:abc.png
+                 */
+
+                sb.append("Content-Disposition: form-data; name=\"img\"; filename=\"" + file.getName() + "\"" + LINE_END);
+                sb.append("Content-Type: application/octet-stream; charset=" + CHARSET + LINE_END);
+                sb.append(LINE_END);
+                dos.write(sb.toString().getBytes());
+                InputStream is = new FileInputStream(file);
+                byte[] bytes = new byte[1024];
+                int len = 0;
+                while ((len = is.read(bytes)) != -1) {
+                    dos.write(bytes, 0, len);
+                }
+                is.close();
+                dos.write(LINE_END.getBytes());
+                byte[] end_data = (PREFIX + BOUNDARY + PREFIX + LINE_END).getBytes();
+                dos.write(end_data);
+                dos.flush();
+                /**
+                 * 获取响应码  200=成功
+                 * 当响应成功，获取响应的流
+                 */
+                int res = conn.getResponseCode();
+                if (res == 200) {
+                    InputStream input = conn.getInputStream();
+                    StringBuffer sb1 = new StringBuffer();
+                    int ss;
+                    while ((ss = input.read()) != -1) {
+                        sb1.append((char) ss);
+                    }
+                    result = sb1.toString();
+                    System.out.println(result);
+                } else {
+                    result = "failure";
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
